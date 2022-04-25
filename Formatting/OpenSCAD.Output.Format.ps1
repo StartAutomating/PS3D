5e3638184ff687dd66bdd599f3d893ea424efba8
James Brundage <@github.com>
2022-04-24 22:34:26 -0700

    Updating formatting (switching to table (#9))

--- Formatting/OpenSCAD.Output.Format.ps1
+++ Formatting/OpenSCAD.Output.Format.ps1
@@ -1,10 +1,1 @@
-Write-FormatView -TypeName OpenSCAD.Output -Action {
-    Write-FormatViewExpression -Text "Arguments"
-
-    Write-FormatViewExpression -ScriptBlock { " $($_.OpenScadArguments -join ' ')" }
-
-    Write-FormatViewExpression -Newline
-
-    Write-FormatViewExpression -Text "Rendering Time" -if { $_.TotalRenderingTime }
-    Write-FormatViewExpression -if { $_.TotalRenderingTime } -ScriptBlock { " $($_.TotalRenderingTime)" }
-} -GroupByProperty OutputPath
+Write-FormatView -TypeName OpenSCAD.Output -Property TotalRenderingTime, Facets, GeometriesInCache -GroupByProperty OutputPath
