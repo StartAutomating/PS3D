fdd1f62344315010777cb82f1c4b39751b3b8a47
James Brundage <@github.com>
2022-04-24 22:22:01 -0700

    Adding formatting for OpenSCAD.Output (#9)

---
+++ Formatting/OpenSCAD.Output.Format.ps1
@@ -0,0 +1,10 @@
+Write-FormatView -TypeName OpenSCAD.Output -Action {
+    Write-FormatViewExpression -Text "Arguments"
+
+    Write-FormatViewExpression -ScriptBlock { " $($_.OpenScadArguments -join ' ')" }
+
+    Write-FormatViewExpression -Newline
+
+    Write-FormatViewExpression -Text "Rendering Time" -if { $_.TotalRenderingTime }
+    Write-FormatViewExpression -if { $_.TotalRenderingTime } -ScriptBlock " $($_.TotalRenderingTime)"
+} -GroupByProperty OutputPath
